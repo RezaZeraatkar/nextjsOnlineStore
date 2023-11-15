4acf0cd34274d1d97c9dcbfee89c26548b97bea2
Reza Zeraatkar <r.zeraatkar1992@gmail.com>
2023-11-15 11:02:01 +0330

    clerk afterAuth middleware added

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,29 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn, clerkClient } from '@clerk/nextjs';
 
 // This middleware protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ['/'],
+  async afterAuth(auth, req, evt) {
+    // handle users who aren't authenticated
+    if (!auth.userId) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+      // return redirectToSignIn({ returnBackUrl: req.url });
+    }
+    console.log(await clerkClient.users.getUser(auth.userId));
+    // redirect them to organization selection page
+    // if (
+    //   auth.userId &&
+    //   !auth.orgId &&
+    //   req.nextUrl.pathname !== '/org-selection'
+    // ) {
+    //   const orgSelection = new URL('/org-selection', req.url);
+    //   return NextResponse.redirect(orgSelection);
+    // }
+  },
 });
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/(api|trpc)(.*)"],
+  matcher: ['/dashboard/:path*', '/(api|trpc)(.*)'],
 };
