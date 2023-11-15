b593d34950e802f248da06212e022864e0fa9897
Reza Zeraatkar <r.zeraatkar1992@gmail.com>
2023-11-15 16:14:47 +0330

    add api/webhook to publicRoutes property

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,12 @@
-import { authMiddleware, redirectToSignIn, clerkClient } from '@clerk/nextjs';
+import { authMiddleware } from '@clerk/nextjs';
 
 // This middleware protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ['/'],
-  async afterAuth(auth, req, evt) {
-    // handle users who aren't authenticated
-    if (!auth.userId) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-      // return redirectToSignIn({ returnBackUrl: req.url });
-    }
-    console.log(await clerkClient.users.getUser(auth.userId));
-    // redirect them to organization selection page
-    // if (
-    //   auth.userId &&
-    //   !auth.orgId &&
-    //   req.nextUrl.pathname !== '/org-selection'
-    // ) {
-    //   const orgSelection = new URL('/org-selection', req.url);
-    //   return NextResponse.redirect(orgSelection);
-    // }
-  },
+  publicRoutes: ['/', '/api/webhook(.*)'],
 });
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
