34963157a4811b3c9640feb34bf2d676019ab9af
Reza Zeraatkar <r.zeraatkar1992@gmail.com>
2023-11-15 16:16:11 +0330

    add clrek web hook api handler to save user in db

---
+++ src/utils/saveUser.ts
@@ -0,0 +1,27 @@
+import dbConnect from '@/services/db/mongoConnection';
+import { IUser } from '@/types/interfaces/user';
+import User from '@/services/db/models/user';
+
+export default async function saveUser(user: any) {
+  try {
+    await dbConnect();
+    console.log(user);
+    const newUser: IUser = {
+      userId: user.id,
+      username: user.username,
+      firstname: user.firstName,
+      lastname: user.lastName,
+      imageUrl: user.imageUrl,
+      emailAddress: 'test',
+    };
+
+    await User.findOneAndUpdate(
+      { userId: user.id }, // filter
+      { $set: newUser }, // update
+      { upsert: true, new: true } // options
+    );
+  } catch (error) {
+    console.error(error);
+    throw new Error('unable to dave user in databse!');
+  }
+}
